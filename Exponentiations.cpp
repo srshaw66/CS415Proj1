bd0305d9adbea509b77a5a1559f9283c1a831d63
Rawkous <121848428+Rawkous@users.noreply.github.com>
2026-09-23 12:42:48 -0700

    Add Exponentiations class with placeholder methods

---
+++ Exponentiations.cpp
@@ -0,0 +1,15 @@
+#include "Exponentiation.h"
+
+using namespace std;
+
+// 
+int Exponentiations::decByOne(int con, int num) {}
+
+// 
+int Exponentiations::decByConst(int con, int num) {}
+
+// 
+int Exponentiations::divConquer(int con, int num) {}
+
+
+
