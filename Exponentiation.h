9898fa85a7194a5ad7c459844ef31d3491396f4e
Rawkous <121848428+Rawkous@users.noreply.github.com>
2026-09-23 12:41:49 -0700

    Add Exponentiation class with basic methods

---
+++ Exponentiation.h
@@ -0,0 +1,16 @@
+#ifndef EXPONENTIATION
+#define EXPONENTIATION  
+
+class Exponentiations {
+    public: 
+
+    // Decrease by one 
+    int decByOne(int a, int m);
+    // Decrease by a constant factor
+    int decByConst(int a, int n);
+    // Divide and Conquer
+    int divConquer(int a, int n);
+
+};
+
+#endif
