ce9beed8f5d56230653c2c1950df8e1f621ad929
Rawkous <121848428+Rawkous@users.noreply.github.com>
2026-09-23 13:09:31 -0700

    Add Sorting class with selection and insertion sort methods

---
+++ Sorting.h
@@ -0,0 +1,14 @@
+#ifndef SORTING
+#define SORTING
+
+class Sorting {
+    public:
+
+    // Selection Sort
+    void selectionSort(int arr[], int n);
+    
+    // Insertion Sort
+    void insertionSort(int arr[], int n);
+};
+
+#endif
